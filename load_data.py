1fb5a42edffa617c7a4d7370f67e74bce39ef7d8
Ho Heon Kim <45510932+4pygmalion@users.noreply.github.com>
2022-11-09 22:51:09 +0900

    issue #6: patch with filter

--- load_data.py
+++ load_data.py
@@ -1,7 +1,11 @@
 import os
 import re
-
 from collections import defaultdict
+
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
 def get_patches(patch_dir:str) -> dict:
     """패치가 저장된 디렉토리를 받아서 이미지 원본소스 별: 패치경로를 반환"""
 
@@ -13,3 +17,13 @@ def get_patches(patch_dir:str) -> dict:
         patch_path[original_image_name].append(os.path.join(patch_dir, patch_name))
 
     return patch_path
+
+
+
+
+def get_multiple_xs(index:int, clinical_array:np.ndarray, all_image_paths:dict) -> tuple:
+    id, image_path, label = clinical_array[index]
+    
+    
+    
+    return  clinical_array[index], all_image_paths[id]
