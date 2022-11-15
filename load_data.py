5dc2e5df8e6ebb7a40c7febcf0e012bcecdde399
Ho Heon Kim <45510932+4pygmalion@users.noreply.github.com>
2022-11-15 23:22:58 +0900

    issue #27: 무한 iteration 수정

--- load_data.py
+++ load_data.py
@@ -14,17 +14,15 @@ def get_patches(patch_dir:str) -> list:
 
 
 def data_generate(bag_names:list, labels:list, bag_dirs:list) -> tuple:
+    for bag_name, label, bag_dir in zip(bag_names, labels, bag_dirs):
+        patch_paths = get_patches(bag_dir)
 
-    while True:
-        for bag_name, label, bag_dir in zip(bag_names, labels, bag_dirs):
-            patch_paths = get_patches(bag_dir)
+        bag_image = list()
+        for patch_path in patch_paths:
+            patch = np.asarray(Image.open(patch_path))
+            bag_image.append(patch)
+            
+        bag_img_tensor = tf.convert_to_tensor(np.stack(bag_image, axis=0))
+        bag_label_tensor = tf.convert_to_tensor(np.array(label).reshape(1, ), dtype=tf.float32)
 
-            bag_image = list()
-            for patch_path in patch_paths:
-                patch = np.asarray(Image.open(patch_path))
-                bag_image.append(patch)
-                
-            bag_img_tensor = tf.convert_to_tensor(np.stack(bag_image, axis=0))
-            bag_label_tensor = tf.convert_to_tensor(np.array(label).reshape(1, ), dtype=tf.float32)
-
-            yield bag_img_tensor, bag_label_tensor
+        yield bag_img_tensor, bag_label_tensor
